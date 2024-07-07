81b2cd35e56015714761c4b8ea8c753fff5781f5
Sarwat Jahan <159925204+SarwatJahan22@users.noreply.github.com>
2024-07-07 16:48:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+var invitations = ["Maria", "Zara"];
+var count_invitations = invitations.length;
+console.log("".concat(count_invitations, " people will come to dinner."));
